ac345890f5a4d035454bc875919c6e63de4c23d7
weishihuai <775334252@qq.com>
2020-01-10 11:09:03 +0800

    Java知识图谱

---
+++ src/main/java/com/wsh/springboot/springbootjavasummary/SpringbootJavaSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.wsh.springboot.springbootjavasummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaSummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJavaSummaryApplication.class, args);
+    }
+
+}
